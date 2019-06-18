456eb6800ee701d67658429b12e2e4475552e0dc
Jeordin Callister <jeordman@Jeordins-MacBook-Air.local>
2019-06-18 15:15:43 -0600

    Mini-done.

--- src/components/FamilyChanger.js
+++ src/components/FamilyChanger.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 export default class FamilyChanger extends Component {
   render() {
-    return (
-      <select className="dropDownContainer">
-        <option value="monospace"> Monospace </option>
-        <option value="arial"> Arial </option>
-        <option value="courier"> Courier </option>
-      </select>
-    );
+      return (
+        <select className="dropDownContainer" onChange={ (e) => this.props.update(e.target.value) } disabled={ this.props.allowEdit === "false" }>
+          <option value="monospace"> Monospace </option>
+          <option value="arial"> Arial </option>
+          <option value="courier"> Courier </option>
+        </select>
+      )
   }
 }
